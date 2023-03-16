e5586a52ceaf3e2dae4cd0296a0ed993f6852ef4
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-03-16 19:36:18 +0200

    commented creation

--- server/db.py
+++ server/db.py
@@ -7,5 +7,5 @@ from server.models.basic_model import db
 db.connect()
 db.create_tables([User, Group, Task])
 
-user = User.create(email="v1@ucu.ua", name="name", surname="surname", password="123")
-group = Group.create(title="aaa", color="red", owner=user)
+# user = User.create(email="v1@ucu.ua", name="name", surname="surname", password="123")
+# group = Group.create(title="aaa", color="red", owner=user)
