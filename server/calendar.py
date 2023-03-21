eac6d8616638d5fb3a53d0df2ef80f5f96ce2bf4
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-03-21 19:07:16 +0200

    pushed minimal api

--- server/calendar.py
+++ server/calendar.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for, request, flash, jsonify
 from server.models.task import Task
+from playhouse.shortcuts import model_to_dict, dict_to_model
 
 calendar_bp = Blueprint('index', __name__)
 
@@ -10,23 +11,29 @@ def calendar():
 
 @calendar_bp.route('/calendar/create', methods = ['POST'])
 def calendar_create():
-    title = request.form.get("title")
-    group_id = request.form.get("group_id")
-    start = request.form.get("start")
-    is_work = request.form.get("is_work")
-    end_time = request.form.get("end_time")
-    end_date = request.form.get("end_date")
+    if request.headers.autherization.split(" ")[1]
+    data = request.json
+    title = data.get("title")
+    group_id = data.get("group_id")
+    start = data.get("start")
+    is_work = data.get("is_work")
+    end_time = data.get("end_time")
+    end_date = data.get("end_date")
 
     overall = None
     if start and end_time:
         overall = end_time - start
+
     task = Task.create(title = title,
-                        description = None,
-                        owner = None,
+                        description = '123',
+                        owner = 1,
                         is_work = is_work,
-                        group = group_id,
+                        group = 1,
                         start_date = start,
                         end_date = end_date,
                         end_time = end_time,
                         overall = overall)
-    return jsonify(task)
+
+    result = model_to_dict(task)
+
+    return jsonify(result)
