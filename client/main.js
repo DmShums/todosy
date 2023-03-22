089dd89527cea62a458bf3170f040080063d9b1b
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-03-22 11:40:56 +0200

    added html create task btn

--- client/main.js
+++ client/main.js
@@ -1,1 +1,97 @@
-(()=>{"use strict";(()=>{const e=document.querySelector("#create-form"),t=document.querySelector("#create-group"),r=document.querySelectorAll(".table__date-tasks"),n=document.querySelectorAll(".task"),c=e=>{const t=document.querySelector(e);t&&t.parentElement.removeChild(t)};r.forEach((r=>{r.addEventListener("click",(t=>{if(!t.target.isSameNode(r))return;t.preventDefault(),c("#create-task");const n=e.content.cloneNode(!0);r.appendChild(n)})),r.addEventListener("contextmenu",(e=>{if(!e.target.isSameNode(r))return;e.preventDefault(),c("#create-group");const n=t.content.cloneNode(!0);r.appendChild(n)}))})),document.addEventListener("keydown",(e=>{"Escape"==e.key&&(c("#create-group"),c("#create-task"))})),n.forEach((e=>{e.addEventListener("click",(t=>{t.preventDefault(),e.classList.toggle("task--edit")}))}))})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+
+
+const createFormTemplate = document.querySelector('#create-form');
+const createGroupTemplate = document.querySelector('#create-group');
+
+const columns = document.querySelectorAll('.table__date-tasks');
+const tasks = document.querySelectorAll('.task');
+
+const deleteBySelector = (selector) => {
+    const prevCreate = document.querySelector(selector);
+
+    if (prevCreate) {
+        prevCreate.parentElement.removeChild(prevCreate);
+    }
+}
+
+const sendAPI = (url, method, body) => {
+    return fetch(url, {
+        method,
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(body)
+    }).then((response) => response.json())
+}
+
+columns.forEach((column) => {
+    column.addEventListener('click', (event) => {
+        if (!event.target.isSameNode(column)) {
+            return;
+        }
+
+        event.preventDefault();
+        deleteBySelector('#create-task');
+        
+        const createForm = createFormTemplate.content.cloneNode(true);
+        createForm.querySelector('form').addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            const target = evt.target;
+            
+            const data = {
+                "title": target.querySelector('#task-title').value,
+                "group_id": target.querySelector('#group-select').value,
+                "is_work": target.querySelector("#is_work").value === 'on',
+                "start": target.querySelector("#start").value || Date.now(),
+                "end_date": '111111',
+                "end_time": target.querySelector("#deadline").value || null,
+            };
+
+            console.log(data)
+            console.log(sendAPI(`/calendar/create`, "POST", data))
+        });
+        column.appendChild(createForm);
+    });
+
+    column.addEventListener('contextmenu', (event) => {
+        if (!event.target.isSameNode(column)) {
+            return;
+        }
+
+        event.preventDefault();
+        deleteBySelector('#create-group');
+
+        const createGroup = createGroupTemplate.content.cloneNode(true);
+        column.appendChild(createGroup);
+    });
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'Escape') {
+        deleteBySelector('#create-group');
+        deleteBySelector('#create-task');
+    }
+})
+
+tasks.forEach((task) => {
+    task.addEventListener('click', (event) => {
+        event.preventDefault();
+        task.classList.toggle('task--edit');
+    });
+});
+
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
--- server/calendar.py
+++ server/calendar.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, url_for, request, jsonify
 from server.models.task import Task
 from playhouse.shortcuts import model_to_dict, dict_to_model
+from datetime import datetime, timedelta, date
+from pprint import pprint
+import json
 
 calendar_bp = Blueprint('index', __name__)
 
@@ -12,6 +15,7 @@ def calendar():
 @calendar_bp.route('/calendar/create', methods = ['POST'])
 def calendar_create():
     data = request.json
+
     title = data.get("title")
     group_id = data.get("group_id")
     start = data.get("start")
@@ -21,18 +25,57 @@ def calendar_create():
 
     overall = None
     if start and end_time:
-        overall = end_time - start
+        start_time = timedelta(hours=int(start.split(':')[0]), minutes=int(start.split(':')[1]))
+        end_time = timedelta(hours=int(end_time.split(':')[0]), minutes=int(end_time.split(':')[1]))
+        overall = end_time - start_time
+        overall = overall.seconds
 
     task = Task.create(title = title,
-                        description = '123',
-                        owner = 1,
-                        is_work = is_work,
-                        group = 1,
-                        start_date = start,
-                        end_date = end_date,
-                        end_time = end_time,
-                        overall = overall)
+                description = '123',
+                owner = 1,
+                is_work = is_work,
+                group = group_id,
+                start_date = start,
+                end_date = end_date,
+                end_time = end_time,
+                overall = overall)
 
     result = model_to_dict(task)
 
-    return jsonify(result)
+    return json.dumps(result, default=str)
+
+
+@calendar_bp.route('/calendar/get', methods=["GET"])
+def calendar_get():
+    data = request.args
+
+    id = data.get(п'id')
+    week = data.get('week')
+
+    dt = datetime.fromtimestamp(int(week) / 1000)
+    start = dt - timedelta(days=dt.weekday())
+    end = start + timedelta(days=7)
+
+    tasks = Task\
+            .select(Task.title, Task.description, Task.group, Task.start_date, Task.end_date)\
+            .where(start <= Task.end_date <= end and Task.owner == 1)\
+            .order_by(Task.end_date)
+
+    pprint({'rows':[model_to_dict(c) for c in tasks]})
+
+    result = ([], [], [], [], [], [], [])
+
+    for task in tasks:
+        result[task.end_date.weekday()].append(model_to_dict(task))
+
+    return json.dumps(result, default=str)
+
+# title = CharField()
+#     description = TextField()
+#     owner = ForeignKeyField(User)
+#     is_work = BooleanField()
+#     group = ForeignKeyField(Group)
+#     start_date = DateTimeField()
+#     end_date = DateField()
+#     end_time = TimeField()
+#     overall = IntegerField()
